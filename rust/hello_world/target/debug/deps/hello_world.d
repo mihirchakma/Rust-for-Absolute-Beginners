acf1b0d1f7db228cacda78a23df8d66e46a664c0
Mihir Chakma <mihirchakmaths@gmail.com>
2024-01-20 09:57:29 +0530

    add hello_world

---
+++ rust/hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\Mihir\Desktop\Rust-for-Absolute-Beginners\rust\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\Mihir\Desktop\Rust-for-Absolute-Beginners\rust\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
